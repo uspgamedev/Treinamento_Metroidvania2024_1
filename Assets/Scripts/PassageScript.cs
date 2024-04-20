2fa8fa730041123e4c54f32eb70677d04cf410f2
Matheus <matheus.feitosa16@outlook.com>
2024-04-19 21:04:58 -0300

    BlackTween ao passar pela porta

--- Assets/Scripts/PassageScript.cs
+++ Assets/Scripts/PassageScript.cs
@@ -2,27 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Adicionando a diretiva using para o namespace UnityEngine.UI
 
 public class PassageScript : MonoBehaviour
 {
     [SerializeField] private string cenaAlvo;
     [SerializeField] private float duracaoFade = 1f;
-
     private bool encostou = false;
     private bool trocarCena = false;
-    private SpriteRenderer fadeRenderer;
-
     private int transFrame = 0;
+    private GameObject blackFade;
+
+    private bool defaded = false;
+
+    private void Start()
+    {
+        blackFade = GameObject.FindGameObjectWithTag("BlackFade");
+        defaded = true;
+        transFrame = 0;
+    }
 
     private void FixedUpdate()
     {
+        if (defaded)
+        {
+            transFrame++;
+            float alpha = Mathf.Lerp(1f, 0f, (float)transFrame / (60 * duracaoFade));
+
+            Image image = blackFade.GetComponent<Image>();
+
+            if (image != null)
+            {
+                image.color = new Color(0f, 0f, 0f, alpha);
+            }
+
+            if (alpha <= 0)
+            {
+                transFrame = 0;
+                defaded = false;
+            }
+        }
+
         if (encostou && !trocarCena)
         {
             transFrame++;
-            float alpha = Mathf.Lerp(255, 0, transFrame / (60 * duracaoFade));
-            
+            float alpha = Mathf.Lerp(0f, 1f, (float) transFrame / (60 * duracaoFade));
 
-            if (alpha >= 250)
+            Image image = blackFade.GetComponent<Image>();
+
+            if (image != null)
+            {
+                image.color = new Color(0f, 0f, 0f, alpha);
+            }
+
+            if (alpha >= 1f)
             {
                 trocarCena = true;
                 SceneManager.LoadScene(cenaAlvo, LoadSceneMode.Single);
