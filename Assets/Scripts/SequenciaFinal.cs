2c7acd27cb83099f4a834051e52b5450a810434f
johntruck <163492528+johntruck@users.noreply.github.com>
2024-06-06 17:03:46 -0300

    a

--- Assets/Scripts/SequenciaFinal.cs
+++ Assets/Scripts/SequenciaFinal.cs
@@ -13,19 +13,19 @@ public class SequenciaFinal : MonoBehaviour
     [SerializeField] private float timerMax = 120f;
     [SerializeField] private float timer = 60f;
     [SerializeField] private GameObject textCanvas;
+    [SerializeField] private GameObject firstTextBox;
+    [SerializeField] private GameObject panelFirstText;
+
     [SerializeField] private GameObject textBox;
     private TextMeshProUGUI text;
-    private GameObject panel1;
+    [SerializeField] private GameObject panel1;
     private SupportScript support;
 
     // Start is called before the first frame update
     void Start()
     {
-        textCanvas = GameObject.Find("CanvasTimer");
-        textBox = textCanvas.transform.Find("Text (TMP)").gameObject;
         text = textBox.GetComponent<TextMeshProUGUI>();
         support = GameObject.Find("ScriptsHelper").GetComponent<SupportScript>();
-        textCanvas.SetActive(false);
     }
 
     // Update is called once per frame
@@ -44,7 +44,6 @@ public class SequenciaFinal : MonoBehaviour
             {
                 GameObject.Find("Player").GetComponent<Health>().currentHealth = 0;
             }
-            //If timer to 0, set player life to zero
         }
     }
 
@@ -57,6 +56,18 @@ public class SequenciaFinal : MonoBehaviour
         }
         GetComponent<BoxCollider2D>().enabled = false;
         textCanvas.SetActive(true);
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText()
+    {
+        panel1.SetActive(false);
+        textBox.SetActive(false);
+        yield return new WaitForSeconds(8);
+        panelFirstText.SetActive(false);
+        firstTextBox.SetActive(false);
+        panel1.SetActive(true);
+        textBox.SetActive(true);
         timer = timerMax;
     }
 }
